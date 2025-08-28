6f45ba96a54553f67c1a4f587de97bdfc60a0ea4
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2025-08-28 14:43:42 +0900

    feat: API 서비스에 코어 모듈 의존성 추가

--- api-service/build.gradle.kts
+++ api-service/build.gradle.kts
@@ -1,12 +1,16 @@
 plugins {
     id("org.springframework.boot")
     id("io.spring.dependency-management")
-    java
     kotlin("jvm") version "2.0.20" // Kotlin 지원
     kotlin("plugin.spring") version "2.0.20" // Spring 과 Kotlin 통합
+    kotlin("plugin.jpa") version "2.0.20" // JPA no-arg 생성자
+    java
 }
 
 dependencies {
+    // Core Module
+    implementation(project(":core"))
+    
     // Kotlin
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -20,11 +24,9 @@ dependencies {
     // DB
     runtimeOnly("com.mysql:mysql-connector-j")
 
-    // Lombok
+    // Lombok (Java에서만 사용)
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
-    testCompileOnly("org.projectlombok:lombok")
-    testAnnotationProcessor("org.projectlombok:lombok")
 
     // Swegger
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
@@ -32,13 +34,20 @@ dependencies {
     // Eureka Service Client - Discovery
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 
-    // Autuator (healthcheck, monitoring)
+    // Actuator (healthcheck, monitoring)
     implementation("org.springframework.boot:spring-boot-starter-actuator")
 
-    // Logging - Log4j2
-    implementation("org.springframework.boot:spring-boot-starter-log4j2")
-
-    // MapStruct
+    // Logging - Log4j2 (기본 로깅 제외)
+    implementation("org.springframework.boot:spring-boot-starter-log4j2") {
+        exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
+    }
+    
+    // 기본 로깅 제외
+    configurations.all {
+        exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
+    }
+    
+    // MapStruct (Java에서만 사용하므로 annotationProcessor만)
     implementation("org.mapstruct:mapstruct:1.5.5.Final")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
 

97af819712842d995904865321bd9a09c4c0b102
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2025-08-28 16:36:30 +0900

    feat: API 서비스에 MapStruct + Lombok 의존성 추가

--- api-service/build.gradle.kts
+++ api-service/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     id("org.springframework.boot")
     id("io.spring.dependency-management")
-    kotlin("jvm") version "2.0.20" // Kotlin 지원
-    kotlin("plugin.spring") version "2.0.20" // Spring 과 Kotlin 통합
-    kotlin("plugin.jpa") version "2.0.20" // JPA no-arg 생성자
+    kotlin("jvm")
+    kotlin("plugin.spring")
+    kotlin("plugin.jpa")
     java
 }
 
@@ -14,42 +14,28 @@ dependencies {
     // Kotlin
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-
-    // Spring Web
+    
+    // Spring Boot Starter
     implementation("org.springframework.boot:spring-boot-starter-web")
-
-    // JPA
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-
-    // DB
-    runtimeOnly("com.mysql:mysql-connector-j")
-
-    // Lombok (Java에서만 사용)
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
-
-    // Swegger
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
 
     // Eureka Service Client - Discovery
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 
-    // Actuator (healthcheck, monitoring)
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    // Database
+    runtimeOnly("mysql:mysql-connector-java:8.0.33")
 
-    // Logging - Log4j2 (기본 로깅 제외)
-    implementation("org.springframework.boot:spring-boot-starter-log4j2") {
-        exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
-    }
-    
-    // 기본 로깅 제외
-    configurations.all {
-        exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
-    }
-    
-    // MapStruct (Java에서만 사용하므로 annotationProcessor만)
+    // Swagger
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+
+    // MapStruct
     implementation("org.mapstruct:mapstruct:1.5.5.Final")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+
+    // Lombok (Java 코드용)
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
 
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")

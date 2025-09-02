73eb4cc8fa7b6567aa3b1b94c4690df51b91facc
이지민 <195277651+1zzimy@users.noreply.github.com>
2025-09-02 09:36:00 +0900

    chore: Jackson 의존성 추가

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -14,4 +14,13 @@ dependencies {
 
     // Swagger
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+
+    // Jackson Core
+    implementation("com.fasterxml.jackson.core:jackson-databind")
+
+    // Kotlin 지원
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    // Java Time (LocalDate, LocalDateTime 직렬화 지원)
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
 }
